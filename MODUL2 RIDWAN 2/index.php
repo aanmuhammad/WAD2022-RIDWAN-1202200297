f1f5716b1b0143e60475e50fa808d5682330a54c
aanmuhammad <115923622+aanmuhammad@users.noreply.github.com>
2022-11-03 23:56:20 +0700

    Add files via upload

---
+++ MODUL2 RIDWAN 2/index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>MODUL2 RIDWAN</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+      integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+      crossorigin="anonymous"
+    />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+
+  
+
+
+
+    <body>
+    <!-- top navbar -->
+    <nav
+      class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg fixed-top"
+    >
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo-ead.png" alt="Logo" width="80" height="24" class="d-inline-block align-text-top"></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <section class="navbar-toggler-icon"></section>
+        </button>
+        <div class="collapse navbar-collapse text-right" id="navbarText">
+          <ul class="navbar-nav ms-auto mb-3 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#tentang">Tentang</a>
+            </li>              
+            <a class="nav-link" href="#lokasi">Lokasi kami</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#jenis">Jenis Mobil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#daftar">Daftar Reservasi</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Tentang Kami -->
+    <div class="container-fluid pt-5 pb-5">
+      <div class="container">
+        <h2 class="display-3 text-center" id="tentang">Tentang Kami</h2>
+        <br>
+        <div class="row justify-content-around">
+          <div class="col-4">
+          EAD Rent Car merupakan jasa penyewaan mobil yang menyediakan berbagai mobil.
+          IT Manager EAD Rent Car ingin mempermudah proses booking persewaan mobil sehingga
+          dapat dilakukan pemesanan penyewaan mobil melalui web.
+
+           </div>
+          <div class="col-4">
+          EAD Rent Car merupakan jasa penyewaan mobil yang menyediakan berbagai mobil.
+          IT Manager EAD Rent Car ingin mempermudah proses booking persewaan mobil sehingga
+          dapat dilakukan pemesanan penyewaan mobil melalui web.
+          </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>   
+    <!-- Lokasi Kami -->
+    <div class="container-fluid lokasi pt-5 pb-5">
+      <div class="container text-center">
+        <h2 class="display-3" id="lokasi">Lokasi Kami</h2>
+        <div class="embed-responsive embed-resposive-16by9">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d990.0703455431056!2d107.6322724292077!3d-6.976090368508438!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e96cdd89acfd%3A0xdb0a0f0d1377b11d!2sGate%204%20TELKOM%20UNIVERSITY!5e0!3m2!1sen!2sid!4v1667493262654!5m2!1sen!2sid" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        
+        <!-- product -->
+    <div class="container-fluid jenis pt-5 pb-5 bg-light">
+      <div class="container text-center">
+        <h2 class="display-3" id="jenis"> Jenis Mobil</h2>
+        <div class="row pt-4 gx-4 gy-4">
+          <div class="col-md-4">
+            <div class="card crop-img">
+              <img
+                src="img/brio.png"
+                class="card-img-top"
+                width="200"
+                height="200"
+              />
+              <div class="card-body">
+                <h5 class="card-title"> HONDA BRIO </h5>
+                <p class="card-text">
+                  Mulai Dari IDK 500.000/ Hari
+                <br><br>
+                Spesifikasi :
+                <ol class="list-group">
+                  <li class="list-group-item">4 Kursi</li>
+                  <li class="list-group-item">1200 cc</li>
+                  <li class="list-group-item">Matic</li>
+                </ol>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card crop-img">
+              <img
+                src="img/Rush.png"
+                class="card-img-top"
+                width="200"
+                height="200"
+              />
+              <div class="card-body">
+                <h5 class="card-title"> TOYOTA RUSH</h5>
+                <p class="card-text">
+                  Mulai Dari IDK 800.000/ Hari
+                  <br><br>
+                  Spesifikasi :
+                  <ol class="list-group">
+                    <li class="list-group-item">5 Kursi</li>
+                    <li class="list-group-item">2000 cc</li>
+                    <li class="list-group-item">Matic</li>
+                  </ol>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card crop-img">
+              <img
+                src="img/ayla.png"
+                class="card-img-top"
+                width="200"
+                height="200"
+              />
+              <div class="card-body">
+                <h5 class="card-title"> TOYOTA AYLA</h5>
+                <p class="card-text">
+                  Mulai Dari IDK 400.000/ Hari
+                  <br><br>
+                  Spesifikasi :
+                  <ol class="list-group">
+                    <li class="list-group-item">4 Kursi</li>
+                    <li class="list-group-item">1200 cc</li>
+                    <li class="list-group-item">Matic</li>
+                  </ol>
+                </p>
+              </div>
+            </div>
+          </div>
+
+<!-- Daftar Mobil -->
+<div class="container-fluid pt-5 pb-5 bg-light">
+      <div class="container text-center">
+        <h2 class="display-3" id="daftar"> Pendaftaran Mobil </h2></div>
+        <form>
+          <div class="mb-3">
+            <label for="Email" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="Email" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text"></div>
+          </div>
+          <div class="mb-3">
+            <label for="Nama" class="form-label">Nama Lengkap</label>
+            <input type="text" class="form-control" id="Nama">
+          </div>
+          <div class="mb-3">
+            <label for="nohp" class="form-label">No Hp</label>
+            <input type="text" class="form-control" id="nohp">
+          </div>
+          <div class="mb-3">
+            <label for="Alamat" class="form-label">Alamat</label>
+            <input type="text" class="form-control" id="Alamat">
+          </div>        
+          <p>Jenis Mobil</p>
+          <select class="form-select" aria-label="Default select example">
+            <option selected>TOYOTA RUSH</option>
+            <option value="1">HONDA BRIO</option>
+            <option value="2">TOYOTA AYLA</option>
+            </select>
+          <br>
+          <p>Driver</p>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+              Ya
+            </label>
+          <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+            <label class="form-check-label" for="flexRadioDefault2">
+              Tidak
+            </label>
+          </div>
+          <br>
+        </form>
+        <button type="submit" class="btn btn-primary">Pesan</button>
+        <br>
+        <br>
+    <!-- Footer -->
+    <footer class="footer text-center bg-light" id="footer">
+      <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Created by Muhammad Ridwan_120220027
+    </button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">!!COPY RIGHT!!
+                    button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-start">
+                    <p>Nama Lengkap : Muhammad Ridwan</p>
+                    <p>NIM : 120220027</p>
+                    <p>Kelas : SI4402</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+            Close
+          </button>
+                </div>
+            </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
